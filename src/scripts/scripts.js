43195bbb608051ed7ad74f488225653c83609144
oriioll <oriolplazas2@gmail.com>
2025-11-13 23:38:00 +0100

    styles v1

---
+++ src/scripts/scripts.js
@@ -0,0 +1,29 @@
+/*
+TO DO:
+Add li to ul with id taskList with the content in the input 
+when button is clicked => button addEventListnerClick -> variable = input.text, create li with id smth with variable text
+
+When task is clicked, variable active to true or false and depending on it, put text in green and underline
+
+
+
+demo:
+let inputText = ""
+function createTask() {
+    inputText = document.getElementWithID('taskInput').text //pick input text
+    create li element with text=inputText
+}
+
+document.getElementWithId('addTaskBtn').addEventListener('Click' => {
+    createTask()    
+})
+    
+
+let li = id or class of li item
+
+li.addEventListener('Click => {
+    li.style.color(var green)
+     li.style.decoration(underline)
+
+    }')
+*/
