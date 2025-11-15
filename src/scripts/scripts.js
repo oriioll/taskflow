b6e1169164a93875a6b5fbe534e4a076620a662f
oriioll <oriolplazas2@gmail.com>
2025-11-15 13:16:23 +0100

    mark task as completed or uncompleted and style it depending on its status

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -28,27 +28,55 @@ li.addEventListener('Click => {
     }')
 */
 
-let inputText = ""
-let newTask = ""
+let inputText = "";
+let newTask = "";
 
+//function to create task based on input text
 function createTask() {
-    inputText = document.getElementById('taskInput').value
-    console.log(inputText)
-    if(inputText.trim() !== "") {
-        newTask = document.createElement('li');
-        newTask.textContent = inputText;
-        document.getElementById('taskList').appendChild(newTask);
-        document.getElementById('taskInput').value = "";
-    }
-    
+  inputText = document.getElementById("taskInput").value;
+  console.log(inputText);
+  if (inputText.trim() !== "") {
+    newTask = document.createElement("li");
+    newTask.textContent = inputText;
+    document.getElementById("taskList").appendChild(newTask);
+    document.getElementById("taskInput").value = "";
+  }
 }
-document.getElementById('addTaskBtn').addEventListener('click', () => {
-  createTask(); 
+
+//function to style li element when its clicked
+
+function completeTask(clickedElement) {
+  let isCompleted = clickedElement.getAttribute("data-is-completed") === "true";
+  if (!isCompleted) {
+    //if task isn't completed, apply completed styles
+    clickedElement.style.textDecoration = "line-through";
+    clickedElement.style.color = "var(--accent-success)";
+    clickedElement.style.opacity = "0.8";
+    //task marked as completed
+    clickedElement.setAttribute("data-is-completed", "true");
+  } else {
+    //if is completed, style and mark as uncompleted
+    clickedElement.style.textDecoration = "none";
+    clickedElement.style.color = "var(--text-primary)";
+    clickedElement.style.opacity = "1";
+    clickedElement.setAttribute("data-is-completed", "false");
+}
+}
+
+document.getElementById("addTaskBtn").addEventListener("click", () => {
+  createTask();
 });
 
 //listens when a key is pressed inside the input and saves it into "event", then checks if that key (event.key) is the enter and if so, creates a task
-document.getElementById('taskInput').addEventListener('keydown', (event) => { 
-  if (event.key === 'Enter') {
-    createTask(); 
+document.getElementById("taskInput").addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    createTask();
   }
-})
+});
+
+//update style when task clicked
+document.getElementById("taskList").addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    completeTask(event.target);
+  }
+});
